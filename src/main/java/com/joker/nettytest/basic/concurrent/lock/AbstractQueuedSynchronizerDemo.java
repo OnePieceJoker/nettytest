2158ad797f4ee7fa99ecfcc07b44006646ce5fb8
lizhihong <joker_lizhih@163.com>
2021-01-27 20:28:52 +0800

    AQS

---
+++ src/main/java/com/joker/nettytest/basic/concurrent/lock/AbstractQueuedSynchronizerDemo.java
@@ -0,0 +1,19 @@
+package com.joker.nettytest.basic.concurrent.lock;
+
+import java.io.Serializable;
+import java.util.concurrent.locks.AbstractOwnableSynchronizer;
+
+/**
+ * AQS，提供了一个框架，用于实现依赖于先进先出（FIFO）等待队列的阻塞锁和相关的同步器（semaphores,events）
+ * 该类旨在为大多数依赖单个原子int值表示状态的同步器提供有用的基础
+ * 
+ * 子类应定义为用于实现其所在类的同步属性的非公共内部帮助器类
+ * 必须定义更改此状态的受保护的方法，定义该状态对于获取（acquired）或释放（released）此对象而言意味着什么
+ * 
+ * 该类支持排他(exclusive mode)模式和共享(shared mode)模式
+ */
+public abstract class AbstractQueuedSynchronizerDemo extends AbstractOwnableSynchronizer implements Serializable {
+
+	private static final long serialVersionUID = -6507747478745427361L;
+    
+}
--- src/test/java/com/joker/nettytest/basic/concurrent/lock/ReentrantLockTest.java
+++ src/test/java/com/joker/nettytest/basic/concurrent/lock/ReentrantLockTest.java
@@ -43,4 +43,110 @@ public class ReentrantLockTest {
         System.out.println(i);
     }
 
+    public static void main(String[] args) {
+        // 通过synchronized来实现同步
+        Student1 student1 = new Student1();
+        Student2 student2 = new Student2();
+        Teacher teacher = new Teacher();
+
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 4; i++) {
+                System.out.println("上课");
+            }
+            teacher.isTeacherFlag();
+            System.out.println("学生1被吵醒了，1s后反应过来");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            student1.setStudent1Flag(true);
+        });
+
+        Thread s1 = new Thread(() -> {
+            student1.isStudent1Flag();
+            System.out.println("准备唤醒学生2，需要1s");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            student2.setStudent2Flag(true);
+        });
+
+        Thread s2 = new Thread(() -> {
+            student2.isStudent2Flag();
+        });
+
+        s1.start();
+        s2.start();
+        t.start();
+    }
+
+    static class Student1 {
+        private boolean student1Flag = false;
+
+        public synchronized boolean isStudent1Flag() {
+            System.out.println("学生1准备睡觉1min");
+            if (!this.student1Flag) {
+                try {
+                    System.out.println("学生1睡着了");
+                    wait(1*1000*60);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("学生1被唤醒");
+            return student1Flag;
+        }
+
+        public synchronized void setStudent1Flag(boolean student1Flag) {
+            this.student1Flag = student1Flag;
+            notify();
+        }
+    }
+    static class Student2 {
+        private boolean student2Flag = false;
+
+        public synchronized boolean isStudent2Flag() {
+            System.out.println("学生2准备睡觉5min");
+            if (!this.student2Flag) {
+                try {
+                    System.out.println("学生2睡着了");
+                    wait(5*1000*60);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("学生2被唤醒");
+            return student2Flag;
+        }
+
+        public synchronized void setStudent2Flag(boolean student2Flag) {
+            notify();
+            this.student2Flag = student2Flag;
+        }
+    }
+
+    static class Teacher {
+        private boolean teacherFlag = true;
+
+        public synchronized boolean isTeacherFlag() {
+            if (!this.teacherFlag) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("老师准备要上课");
+            return teacherFlag;
+        }
+
+        public synchronized void setTeacherFlag(boolean teacherFlag) {
+            this.teacherFlag = teacherFlag;
+            notify();
+        }
+    }
+
 }
